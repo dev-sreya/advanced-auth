4775e09a168ff94fbe0f9f35c509ce6ade58d325
Sreya Karmakar <sreyaworldz@gmail.com>
2025-06-18 03:36:09 +0530

    Added routes signup, login, logout

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup controller working");
+};
+
+export const login = async (req, res) => {
+  res.send("Login controller working");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout controller working");
+};
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js"
+
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
     res.send("hello World it's ME 321");
 })
 
-app.listen(3000, () => {
+app.use("/api/auth", authRoutes)
+
+app.listen(5000, () => {
     connectDB();
-    console.log("Server is running on ort 3000");
+    console.log("Server is running on port:", PORT);
 });
 
-//qU5Yn7sxWASNjKj3
